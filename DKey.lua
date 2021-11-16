af67fcfb7c5c96d1160f89efc8a52e6c0618a796
RamiN339 <91587352+RamiN339@users.noreply.github.com>
2021-11-15 17:36:36 -0800

    Create DKey.lua

---
+++ DKey.lua
@@ -0,0 +1,32 @@
+UserInputService = game:GetService("UserInputService")
+local Button = script.Parent
+WButton = false
+
+UserInputService.InputBegan:Connect(function(input, GameProccesedEvent)
+	if input.KeyCode == Enum.KeyCode.D then
+		WButton = true
+		while WButton == true do
+			wait()
+			--Button.BackgroundTransparency = 1
+			Button.TextStrokeTransparency = 0
+			Button.TextStrokeColor3 = Color3.new(0, 1, 0)
+
+		end
+
+
+	end
+end)
+
+UserInputService.InputEnded:Connect(function(input, GameProccesedEvent)
+	if input.KeyCode == Enum.KeyCode.D then
+		WButton = false
+		while WButton == false do
+			wait()
+			--Button.BackgroundTransparency = 0
+			Button.TextStrokeColor3 = Color3.new(1, 0, 0)
+
+		end
+	end
+
+
+end)
